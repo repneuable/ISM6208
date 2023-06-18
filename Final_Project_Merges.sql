a36f2986c7f102b719b4b631a4cf146e1644815d
Kevin <32113593+repneuable@users.noreply.github.com>
2023-06-18 04:10:08 -0400

    Update Final_Project_Merges.sql

--- Final_Project_Merges.sql
+++ Final_Project_Merges.sql
@@ -1,6 +1,6 @@
 ------------------------------------------------------------
 ------------------------------------------------------------
--- Title:    Final Project Data Importing & Cleaning
+--@Title:    Final Project Data Importing & Cleaning
 -- Course:   ISM6208.020U23
 -- Date:     June 2023
 -- Authors:  Dalton Anderson & Kevin Hitt 
@@ -39,6 +39,9 @@ SELECT * FROM PCE_BQ WHERE ROWNUM < 11;
 SELECT * FROM US_HH_DEBT_GDP WHERE ROWNUM < 11;
 SELECT * FROM REAL_GDP_PER_CAPITA WHERE ROWNUM < 11;
 SELECT * FROM REAL_GDP WHERE ROWNUM < 11;
+
+SELECT * FROM GDP_MERGED WHERE ROWNUM < 11;
+SELECT * FROM GDP_MERGED_BQ WHERE ROWNUM < 11;
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 --  FED Rate
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -223,6 +226,34 @@ SELECT * FROM GDP_MERGED_BQ;
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 --  FED Rate
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SELECT 
+  RECORD_DATE,
+  MAX(Value_mtg_avg_30) as Value_mtg_avg_30, 
+  MAX(Value_real_inr_rate_10yr) as Value_real_inr_rate_10yr, 
+  MAX(Value_fed_debt) as Value_fed_debt, 
+  MAX(Value_fed_funds_eff_rate) as Value_fed_funds_eff_rate, 
+  MAX(Value_int_rate_reserve) as Value_int_rate_reserve, 
+  MAX(Value_treasury_yield) as Value_treasury_yield
+FROM (
+  SELECT RECORD_DATE, MORTGAGE30US as Value_mtg_avg_30, NULL as Value_real_inr_rate_10yr, NULL as Value_fed_debt, NULL as Value_fed_funds_eff_rate, NULL as Value_int_rate_reserve, NULL as Value_treasury_yield FROM MTG_AVG_30YR_FIXED_RATE_US
+  UNION ALL
+  SELECT RECORD_DATE, NULL as Value_mtg_avg_30, REAINTRATREARAT10Y as Value_real_inr_rate_10yr, NULL as Value_fed_debt, NULL as Value_fed_funds_eff_rate, NULL as Value_int_rate_reserve, NULL as Value_treasury_yield FROM REAL_INTEREST_RATE_10YR
+  UNION ALL
+  SELECT RECORD_DATE, NULL as Value_mtg_avg_30, NULL as Value_real_inr_rate_10yr, GFDEBTN as Value_fed_debt, NULL as Value_fed_funds_eff_rate, NULL as Value_int_rate_reserve, NULL as Value_treasury_yield FROM FED_DEBT_TOTAL_PUBLIC_DEBT
+  UNION ALL
+  SELECT RECORD_DATE, NULL as Value_mtg_avg_30, NULL as Value_real_inr_rate_10yr, NULL as Value_fed_debt, DFF as Value_fed_funds_eff_rate, NULL as Value_int_rate_reserve, NULL as Value_treasury_yield FROM FED_FUNDS_EFFECTIVE_RATE
+  UNION ALL
+  SELECT RECORD_DATE, NULL as Value_mtg_avg_30, NULL as Value_real_inr_rate_10yr, NULL as Value_fed_debt, NULL as Value_fed_funds_eff_rate, IORB as Value_int_rate_reserve, NULL as Value_treasury_yield FROM INT_RATE_RESERVE_BAL
+  UNION ALL
+  SELECT RECORD_DATE, NULL as Value_mtg_avg_30, NULL as Value_real_inr_rate_10yr, NULL as Value_fed_debt, NULL as Value_fed_funds_eff_rate, NULL as Value_int_rate_reserve, DGS10 as Value_treasury_yield FROM TREASURY_YIELD_10YR
+) 
+GROUP BY RECORD_DATE;
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Create new table with FISCAL QUARTER
+CREATE TABLE FED_RATE_MERGED_BQ AS SELECT * FROM FED_RATE_MERGED;
+ALTER TABLE FED_RATE_MERGED_BQ ADD (FISCAL_QUARTER VARCHAR2(5));
+UPDATE FED_RATE_MERGED_BQ SET FISCAL_QUARTER = 'Q' || TO_CHAR(RECORD_DATE, 'Q');
+SELECT * FROM FED_RATE_MERGED_BQ ORDER BY RECORD_DATE;
 
 -- *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
